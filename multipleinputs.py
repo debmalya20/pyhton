20177660c2a78892b99f5bcd9ad620a61b589b9c
Debmalya <db0668191@gmail.com>
2026-06-04 23:28:55 +0530

    This is decorators,*args

---
+++ multipleinputs.py
@@ -0,0 +1,23 @@
+#*args
+#suppose i created a function of 2 peremeters and i want to add more the 2 parameters. The  we use *args
+
+# def addition(a,b):
+#     print(a+b)
+# addition(12,45)
+
+#now if i want more then 2 veriables
+#When we create a *args it cretas a tupple 
+
+def addition(*args):
+    sum=0
+    for i in args:
+        sum=sum+i
+    print(sum)
+
+
+
+addition(12,45,766,34)
+
+
+
+    
