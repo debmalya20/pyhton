3a3add4c589471674b2bc0b6f0f7442ed651d5f1
Debmalya <db0668191@gmail.com>
2026-06-04 23:12:46 +0530

    This all about make my own decorator

--- decorattor.py
+++ decorattor.py
@@ -1,9 +1,29 @@
-class animal:
-    @property 
-    #if you use this property decorattor then we dont have tp us bracket while calling the method
+# class animal:
+#     @property 
+#     #if you use this property decorattor then we dont have tp us bracket while calling the method
 
-    def sound(self):
-        print("Hukka huaaaa")
+#     def sound(self):
+#         print("Hukka huaaaa")
 
-obj=animal()
-obj.sound
+# obj=animal()
+# obj.sound
+
+# creating my own decarator
+
+def callme(function):
+    def wrapper(a,b):
+        print("This is how we can create out own decorate")
+        function(a,b) #this function will take me() function
+        print("Then this will print")
+    return wrapper
+
+
+@callme
+
+def addition(a,b):
+   print(f"The sum of your numbers are={a+b}")
+
+addition(10,22)
+    
+
+
